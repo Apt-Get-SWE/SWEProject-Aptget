61cc58e0aa5313b2454f53da76919f5602210a92
Leo Li <leo4life@163.com>
2022-10-05 17:13:25 -0400

    test heroku

--- server/app.py
+++ server/app.py
@@ -4,16 +4,16 @@ The endpoint called `endpoints` will return all available endpoints.
 """
 
 from flask import Flask, send_from_directory
-from flask_restful import Api
-from .src.endpoints.endpoints import Endpoints
+from flask_restful import Api, Resource
+from .src.endpoints.endpoints import Endpoints, HelloWorld
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 api = Api(app)
 
+class Index(Resource):
+    def get(self):
+        return send_from_directory(app.static_folder, 'index.html')
 
-@app.route("/", defaults={'path': ''})
-def serve(path):
-    return send_from_directory(app.static_folder, 'index.html')
-
-
+api.add_resource(Index, "/")
 api.add_resource(Endpoints, "/endpoints")
+api.add_resource(HelloWorld, "/hello")
