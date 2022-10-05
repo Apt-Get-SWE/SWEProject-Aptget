f0c33cfb408bc029f39f1ae46fd5b0e3012fd0d6
Leo Li <leo4life@163.com>
2022-10-05 14:21:27 -0400

    fix test

--- server/app.py
+++ server/app.py
@@ -4,14 +4,16 @@ The endpoint called `endpoints` will return all available endpoints.
 """
 
 from flask import Flask, send_from_directory
-from flask_restful import Resource, Api
+from flask_restful import Api
 from .src.endpoints.endpoints import Endpoints
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 api = Api(app)
 
-@app.route("/", defaults={'path':''})
+
+@app.route("/", defaults={'path': ''})
 def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
+
 
 api.add_resource(Endpoints, "/endpoints")
