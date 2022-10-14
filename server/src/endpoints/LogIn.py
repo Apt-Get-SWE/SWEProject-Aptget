58378f6b4af8dbbf4c76416a2cf96602f675c628
Oliver Li <oliverli@10-18-92-120.dynapool.wireless.nyu.edu>
2022-10-14 11:45:24 -0400

    Updated redirect uri and added skeleton functions for user verification

--- server/src/endpoints/LogIn.py
+++ server/src/endpoints/LogIn.py
@@ -1,6 +1,6 @@
 import os
 import pathlib
-from flask import redirect
+from flask import redirect, request, url_for
 from flask_restx import Resource
 from google_auth_oauthlib.flow import Flow
 
@@ -9,24 +9,40 @@ CLIENT_SECRET_FILE = os.path.join(pathlib.Path(__file__).parents[2], "client_sec
 flow = Flow.from_client_secrets_file(
     client_secrets_file=CLIENT_SECRET_FILE,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:8000/loggedin"
+    redirect_uri="http://127.0.0.1:8000/callback"
     )
 
 class GoogleLogIn(Resource):
+    def __init__(self, api=None, *args, **kwargs):
+        super().__init__(api, *args, **kwargs)
+        self.session = {}
     """
     Calls google auth api to authenticate google user log in 
     """
     def get(self):
         """
         The url to jump to needs to be manually added to the google credentials. 
-        #TODO: store state in sessions to track logged in users and parse user info to store to db. 
+        #TODO: store state in sessions to track logged in users and parse user info to store to db. Verify if 
         """
         authorizationUrl, state = flow.authorization_url()
-        return redirect(authorizationUrl)
+        if state: # check
+            return redirect(authorizationUrl)
+        else:
+            return redirect("http://127.0.0.1:8000/endpoints")
+
+class VerifyUserLogin(Resource):
+    
+    def get(self):
+        """
+        Call back, verify if users exists. Prompt for new user to register
+        """
+        return {"verify":"stuff"}
+    
 
 class LogInSuccessPage(Resource):
     """
     Place holder for the page to jump to after log in is successful.
     """
     def get(self):
+        print(request.args["user"])
         return {"Login": "Successful"}
