8ac1016bd16609d768daa64af106bc901634b07e
TomY-Zhang <tyz212@nyu.edu>
2022-10-23 14:28:30 -0400

    Suppport for local mongodb instance
    Edited query methods to support local db queries

--- server/src/db/query.py
+++ server/src/db/query.py
@@ -3,28 +3,44 @@ from pymongo import MongoClient, collection
 
 COLLECTIONS = ['posts', 'items', 'users', 'addresses']
 
-def get_collection(dbname: str, collection_name: str) -> collection.Collection:
-    uri = os.getenv('DB_URI')
-    if uri is None:
-        raise ValueError('DB_URI environemnt variable not set!')
-    client = MongoClient(uri)
+def get_collection(dbname: str, collection_name: str, local=False) -> collection.Collection:
+    client = None # to be connect to local/remote MongoDB
+
+    # if local is True, connect to local db, otherwise connect to remote db
+    if local:
+        client = MongoClient('localhost', 27017) # connect to local db on port 27017
+    else:
+        uri = os.getenv('DB_URI') # get remote db URI
+        if uri is None:
+            raise ValueError('DB_URI environemnt variable not set!')
+        client = MongoClient(uri) # connect to remote db
+    
+    # get db and specified collection
     return client[dbname][collection_name]
 
-def insert(collection_name: str, data: dict) -> None:
+# insert document / JSON object to specified collection
+def insert(collection_name: str, data: dict, local=False) -> None:
     if collection_name.lower() not in COLLECTIONS:
         raise Exception(f"Cannot insert to '{collection_name}'")
-    collection = get_collection('apt-get', collection_name)
+
+    # fetch collection from db & insert data to collection
+    collection = get_collection('apt-get', collection_name, local)
     collection.insert_one(data)
 
-def find_all(collection_name: str, filters={}) -> list:
+# find all instances of document / JSON object in specified collection
+def find_all(collection_name: str, filters={}, local=False) -> list:
     if collection_name.lower() not in COLLECTIONS:
         raise Exception(f"Cannot retrieve from '{collection_name}'")
-    collection = get_collection('apt-get', collection_name)
+
+    # fetch collcction from db & return all desired documents in a list
+    collection = get_collection('apt-get', collection_name, local)
     return list(collection.find(filters))
 
-def find_one(collection_name: str, filters={}) -> dict:
+# find one instance of document / JSON object in specified collection
+def find_one(collection_name: str, filters={}, local=False) -> dict:
     if collection_name.lower() not in COLLECTIONS:
         raise Exception(f"Cannot retrieve from '{collection_name}'")
-    collection = get_collection('apt-get', collection_name)
+
+    # fetch collection from db & return first instance of desired document
+    collection = get_collection('apt-get', collection_name, local)
     return collection.find_one(filters)
-    
--- server/src/types/types.py
+++ server/src/types/types.py
@@ -17,16 +17,16 @@ User format
 """
 class User:
     # STATIC METHODS
-    def insert(data: dict) -> None:
+    def insert(data: dict, local=False) -> None:
         if type(data) != dict:
             raise ValueError(f'Cannot insert data of type{type(data)}')
-        query.insert('users', data)
+        query.insert('users', data, local)
 
-    def find_all(filters={}) -> list:
-        return query.find_all('users', filters)
+    def find_all(filters={}, local=False) -> list:
+        return query.find_all('users', filters, local)
 
-    def find_one(filters={}) -> dict:
-        return query.find_one('users', filters)
+    def find_one(filters={}, local=False) -> dict:
+        return query.find_one('users', filters, local)
 
     # NON-STATIC METHODS
     def __init__(self, data: dict):
@@ -48,16 +48,16 @@ Address format
 """
 class Address:
     # STATIC METHODS
-    def insert(data: dict) -> None:
+    def insert(data: dict, local=False) -> None:
         if type(data) != dict:
             raise ValueError(f'Cannot insert data of type{type(data)}')
-        query.insert('addresses', data)
+        query.insert('addresses', data, local)
 
-    def find_all(filters={}) -> list:
-        return query.find_all('addresses', filters)
+    def find_all(filters={}, local=False) -> list:
+        return query.find_all('addresses', filters, local)
 
-    def find_one(filters={}) -> dict:
-        return query.find_one('addresses', filters)
+    def find_one(filters={}, local=False) -> dict:
+        return query.find_one('addresses', filters, local)
 
     # NON-STATIC METHODS
     def __init__(self, data: dict):
@@ -81,16 +81,16 @@ Post format
 """
 class Post:
     # STATIC METHODS
-    def insert(data: dict) -> None:
+    def insert(data: dict, local=False) -> None:
         if type(data) != dict:
             raise ValueError(f'Cannot insert data of type{type(data)}')
-        query.insert('posts', data)
+        query.insert('posts', data, local)
     
-    def find_all(filters={}) -> list:
-        return query.find_all('posts', filters)
+    def find_all(filters={}, local=False) -> list:
+        return query.find_all('posts', filters, local)
 
-    def find_one(filters={}) -> dict:
-        return query.find_one('posts', filters)
+    def find_one(filters={}, local=False) -> dict:
+        return query.find_one('posts', filters, local)
     
     # NON-STATIC METHODS
     def __init__(self, data: dict):
