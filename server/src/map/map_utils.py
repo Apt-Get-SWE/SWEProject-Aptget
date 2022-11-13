b78f7479f20857bcb94611d62e86afdf68585a33
Oliver Li <oliverli@Olivers-MacBook-Pro.local>
2022-11-13 18:03:33 -0500

    Added serialization function for google api calls

--- server/src/map/map_utils.py
+++ server/src/map/map_utils.py
@@ -3,6 +3,37 @@ import googlemaps
 
 GOOGLE_API_KEY = "AIzaSyCjz6CWyd29cNaI7mLbPzDbhlBR_aIpLp4" #TODO set as environment variable
 
+def makeRequest():
+    """
+    Make https requests to Google API
+
+    Method prepares url parameters, drops None values, and gets default
+    values. Finally makes request using protocol assigned to client and
+    returns data.
+
+    :param url: url part - specifies API endpoint
+    :param parameters: dictionary of url parameters
+    :param result_key: key in output where result is expected
+    """
+    #TODO: implement make request
+    ...
+
+def serializeParameters(parameters):
+    """
+    Serialize parameters from python native types to match that specified in google api docs
+    
+    parameters: dictionary of query parameters
+    """
+    serializedParam = {}
+    for key, value in parameters.items():
+        if isinstance(value, bool):
+            serializeParameters[key] = 'true' if value else 'false'
+        elif isinstance(value, dict):
+            serializedParam[key] = "|".join(("%s:%s" % (k, v) for k, v in value.items()))
+        elif isinstance(value, (list, tuple)):
+            parameters[key] = "|".join(value)
+    return serializedParam
+
 def encodeAddress(address: str):
     gmap = googlemaps.Client(key=GOOGLE_API_KEY)
     geocode = gmap.geocode(address)
