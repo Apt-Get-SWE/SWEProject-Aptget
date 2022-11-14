896defa2fb52626429da10ff76a4f7954f9a0905
Leo Li <leo4life@163.com>
2022-11-13 20:01:18 -0500

    tests

--- server/tst/endpoints/test_addresses.py
+++ server/tst/endpoints/test_addresses.py
@@ -1,11 +1,19 @@
+import flask
 from ...src.endpoints.addresses import Addresses
 
 class TestPosts:
     def test_query(self):
         return # TODO: setup testing local db
         # Test with a valid query
-        addr = Addresses()
-        response = addr.get()
-        assert response['Type'] == 'Data'
-        assert response['Title'] == 'List of addresses'
-        assert response['Data'] != None
+        with flask.Flask(__name__).test_request_context() as flask_context:
+            flask_context.request.args = {"zip": "11201"}
+
+            addr = Addresses()
+            response = addr.get()
+            assert response['Type'] == 'Data'
+            assert response['Title'] == 'List of addresses'
+            assert response['Data'] != None
+            
+            # Assert zip code is correct
+            for aid, addr in response['Data'].items():
+                assert addr['zipcode'] == '11201'
