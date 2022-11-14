9bf0148a7856962614074de8775496df7b011f6e
Leo Li <leo4life@163.com>
2022-11-13 19:42:19 -0500

    support filter by zip

--- server/src/endpoints/addresses.py
+++ server/src/endpoints/addresses.py
@@ -1,12 +1,18 @@
-from flask_restful import Resource
+from flask_restful import Resource, request
 from ..types.address import Address
 from ..types.utils import parse_json
 
 class Addresses(Resource):
     def get(self):
         data = parse_json(Address.find_all())
+        
+        # If zip code url param is provided, filter by zip code
+        zipcode = request.args.get('zip')
+
         formatted_data = {}
         for addr in data:
+            if zipcode and addr['zipcode'] != zipcode:
+                continue
             formatted_data[addr['aid']] = addr
 
         return {
