6e366ec44305606741e3f19429a3b72692851447
Oliver Li <oliverli@Olivers-MacBook-Pro.local>
2022-11-20 13:25:27 -0500

    Added basic time util functions, need to combine with get timezone from google maps api

---
+++ server/src/time/time_utils.py
@@ -0,0 +1,12 @@
+import time
+import datetime
+
+def get_current_epoch():
+    """
+    Get timestamp epoch value. This should be the value stored in db. 
+    Convert to regular datetime based on timezone for display. 
+    """
+    return int(time.time())
+
+def epoch_to_datetime(timestampEpoch):
+    return datetime.datetime.fromtimestamp(timestampEpoch)
---
+++ server/tst/time/test_time_utils.py
@@ -0,0 +1,16 @@
+from ...src.time.time_utils import get_current_epoch, epoch_to_datetime
+
+class TestTimeUtils:
+    def test_get_current_epoch(self):
+        timestampEpoch1 = get_current_epoch()
+        timestampEpoch2 = get_current_epoch()
+        assert timestampEpoch1 <= timestampEpoch2
+    
+    def test_epoch_to_datetime(self):
+        timestampEpoch = 40246871
+        date_time = epoch_to_datetime(timestampEpoch)
+        assert date_time.year == 1971
+        assert date_time.month == 4
+        assert date_time.day == 11
+        assert date_time.hour == 14
+        assert date_time.minute == 41
