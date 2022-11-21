bf2549b573943caa38040af0a918578f88e77304
Leo Li <leo4life@163.com>
2022-11-20 20:49:07 -0500

    impl

--- server/src/endpoints/posts.py
+++ server/src/endpoints/posts.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from flask import request
 from ..types.post import Post
 from ..types.utils import parse_json
 
@@ -14,3 +15,19 @@ class Posts(Resource):
             'Title' : 'List of posts',
             'Data' : formatted_data
         }
+
+    def post(self):
+        # For creating a new post
+        content_type = request.headers.get('Content-Type')
+        if content_type == 'application/json':
+            json = request.json
+        else:
+            return 'Content-Type not supported!', 415
+
+        # Parse pid, aid, uid, title, descr, condition, price, sold from json
+        post = Post.from_json(json)
+        try:
+            post.save()
+            return "Post created successfully", 201
+        except Exception as e:
+            return f'Error saving post: {e}', 500
