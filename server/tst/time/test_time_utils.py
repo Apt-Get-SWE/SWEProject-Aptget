501bc0c9d9ff44f9d002c4a211e0587dd26d51c2
Leo Li <leo4life@163.com>
2022-11-30 02:11:59 -0500

    second fixture

--- server/tst/time/test_time_utils.py
+++ server/tst/time/test_time_utils.py
@@ -1,13 +1,17 @@
 from ...src.time.time_utils import get_current_epoch, epoch_to_datetime
 import pytest
 class TestTimeUtils:
+    @pytest.fixture
+    def get_test_epoch(self):
+        return 40246871
+
     def test_get_current_epoch(self):
         timestampEpoch1 = get_current_epoch()
         timestampEpoch2 = get_current_epoch()
         assert timestampEpoch1 <= timestampEpoch2
 
-    def test_epoch_to_datetime(self):
-        timestampEpoch = 40246871
+    def test_epoch_to_datetime(self, get_test_epoch):
+        timestampEpoch = get_test_epoch
         date_time = epoch_to_datetime(timestampEpoch)
         assert date_time.year == 1971
         assert date_time.month == 4
