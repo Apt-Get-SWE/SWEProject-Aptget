cc4e7df7f57f3b55fc303131ead13e9d1468b810
Leo Li <leo4life@163.com>
2023-02-05 22:47:18 -0500

    update server

--- server/frontend/server.js
+++ server/frontend/server.js
@@ -6,12 +6,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 const port = process.env.PORT || 8080;
 
 var apiProxy = createProxyMiddleware('/api', { target: 'http://0.0.0.0:8000'});
-var swaggerProxy = createProxyMiddleware('/swaggerui', { target: 'http://0.0.0.0:8000'});
-var frontendProxy = createProxyMiddleware('/', { target: 'http://localhost:3000' });
-
 app.use(apiProxy);
-app.use(swaggerProxy);
-app.use(frontendProxy);
+
+// Serve the built frontend files
+app.use(express.static(path.join(__dirname, '/build')));
+
+// Serve the index.html file for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
