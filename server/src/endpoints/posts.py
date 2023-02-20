e482e72bc8e8db2354b73d891e86b661fb7181da
Leo Li <leo4life@163.com>
2023-02-19 20:17:57 -0500

    add tests

--- server/src/endpoints/posts.py
+++ server/src/endpoints/posts.py
@@ -69,10 +69,10 @@ class Posts(Resource):
         try:
             post = Post.from_json(json)
             cookie_user_id = session.get("user_id")
-            
+
             if cookie_user_id is None:
                 return "User not logged in", 401
-            
+
             if cookie_user_id != post.uid:
                 return "User does not own post", 401
 
@@ -137,7 +137,7 @@ class Posts(Resource):
 
             if cookie_user_id is None:
                 return "User not logged in", 401
-            
+
             if cookie_user_id != post.uid:
                 return "User does not own post", 401
 
--- server/tst/endpoints/test_posts.py
+++ server/tst/endpoints/test_posts.py
@@ -12,6 +12,10 @@ class TestPosts:
         if os.getenv('CLOUD') == q.LOCAL:
             q.delete_all('posts', {})
         with app.test_client() as client:
+            # set client session user_id cookie to 1337
+            with client.session_transaction() as sess:
+                sess['user_id'] = '1337'
+
             yield client
         if os.getenv('CLOUD') == q.LOCAL:
             q.delete_all('posts', {})
@@ -24,7 +28,7 @@ class TestPosts:
 
     def test_get(self, client):
         if os.getenv('CLOUD') == q.LOCAL:
-            newpost = Post('1', '1', '1', 'Local test first post',
+            newpost = Post('1', '1337', '1', 'Local test first post',
                            'First test descr', 'new', '1668994811', '100', 'False')
             newpost.save()
 
@@ -38,6 +42,42 @@ class TestPosts:
             response = client.delete('api/posts/posts', json={'pid': '1'})
             assert response.status_code == 201
 
+    def test_post_no_login_fail(self, client):
+        if os.getenv('CLOUD') == q.LOCAL:
+            with client.session_transaction() as sess:
+                sess['user_id'] = None
+
+            response = client.post('api/posts/posts', json={
+                'pid': '2',
+                'uid': '1337',
+                'aid': '2',
+                'title': 'Local test second post',
+                'descr': 'Second test descr',
+                'condition': 'new',
+                'list_dt': '1668994811',
+                'price': '100',
+                'sold': 'False',
+            })
+
+            assert response.status_code == 401
+
+    def test_post_loggedin_uid_not_match_fail(self, client):
+        if os.getenv('CLOUD') == q.LOCAL:
+
+            response = client.post('api/posts/posts', json={
+                'pid': '2',
+                'uid': '1338',
+                'aid': '2',
+                'title': 'Local test second post',
+                'descr': 'Second test descr',
+                'condition': 'new',
+                'list_dt': '1668994811',
+                'price': '100',
+                'sold': 'False',
+            })
+
+            assert response.status_code == 401
+
     def test_post_fail(self, client):
         if os.getenv('CLOUD') == q.LOCAL:
             response = client.post('api/posts/posts', json={
@@ -49,7 +89,7 @@ class TestPosts:
         if os.getenv('CLOUD') == q.LOCAL:
             response = client.post('api/posts/posts', json={
                 'pid': '2',
-                'uid': '2',
+                'uid': '1337',
                 'aid': '2',
                 'title': 'Local test second post',
                 'descr': 'Second test descr',
@@ -86,7 +126,7 @@ class TestPosts:
 
             response = client.put('api/posts/posts', json={
                 'pid': '1',
-                'uid': '1',
+                'uid': '1337',
                 'aid': '1',
                 'title': 'Updated local test first post',
                 'descr': 'test descr',
@@ -115,7 +155,7 @@ class TestPosts:
 
     def test_delete(self, client):
         if os.getenv('CLOUD') == q.LOCAL:
-            newpost = Post('1', '1', '1', 'Local test first post',
+            newpost = Post('1', '1337', '1', 'Local test first post',
                            'First test descr', 'new', '1668994811', '100', 'False')
             newpost.save()
 
