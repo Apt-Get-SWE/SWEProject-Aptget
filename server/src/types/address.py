1ea93962156510e2dbba59a3eadb7893384ed8ee
TomY-Zhang <tyz212@nyu.edu>
2023-02-25 16:38:30 -0500

    documented methods and updated Post.delete() to delete_one()

--- server/src/types/address.py
+++ server/src/types/address.py
@@ -5,22 +5,34 @@ import usaddress
 import json
 import logging
 
-"""
-Address format
-{
-    aid      : str,
-    building : str,
-    city     : str,
-    state    : str, # store abbreviated name, i.e. NY = New York
-    zipcode  : str,
-}
-"""
-
 
 class Address:
+    """
+    Required fields:
+    {
+        aid      : str
+        building : str
+        city     : str
+        state    : str (abbreviated name, i.e. NY = New York)
+        zipcode  : str
+    }
+    """
+
     # STATIC METHODS
     @staticmethod
     def insert(data: dict) -> None:
+        """
+        Inserts new Address to database or updates Address if
+        same AID is found.
+
+        Arguments:
+        data (dict) -- dict containing the Address information
+
+        Exceptions:
+        ValueError -- raised if data is not of type dictionary or if the
+                        dictionary does not contain required fields
+        """
+
         if type(data) != dict:
             raise TypeError(f'Cannot insert data of type{type(data)}')
 
@@ -38,40 +50,64 @@ class Address:
 
     @staticmethod
     def update(filters: dict, new_values: dict) -> None:
+        """
+        Finds a single Address with the specified filters
+        and updates them with new values.
+
+        Arguments:
+        filters    -- the Address attributes to search for
+        new_values -- the data to be added to the Address
+
+        Exceptions:
+        TypeError -- raised if filters or new_values are not of type dict
+        """
         if type(new_values) != dict:
-            raise TypeError(f'Cannot update with data of type{type(new_values)}') # noqa
+            raise TypeError(f'Cannot update with data of type{type(new_values)}')  # noqa
         if type(filters) != dict:
-            raise TypeError(f'Cannot update with filters of type{type(filters)}') # noqa
+            raise TypeError(f'Cannot update with filters of type{type(filters)}')  # noqa
 
         query.update('addresses', filters, new_values)
-        logging.info(f'Updated users w/ filters {filters}')
+        logging.info(f'Updated Addresses w/ filters {filters}')
 
     @staticmethod
     def find_all(filters={}) -> list:
+        """Returns a list of all Addresss found with the filters provided ."""
         return query.find_all('addresses', filters)
 
     @staticmethod
     def find_one(filters={}) -> dict:
+        """Returns a single Address dict with the filters provided."""
         return query.find_one('addresses', filters)
 
     @staticmethod
     def exists(filters={}) -> bool:
+        """Counts all Addresses with filters provided."""
         return query.exists('addresses', filters)
 
     @staticmethod
     def count(filters={}) -> int:
+        """Counts all Addresses with filters provided."""
         return query.count('addresses', filters)
 
     @staticmethod
     def delete_one(filters={}) -> results.DeleteResult:
+        """
+        Finds and deletes a single Address with the filters provided.
+        Returns the deleted data as a DeleteResult.
+        """
         return query.delete_one('addresses', filters)
 
     @staticmethod
     def delete_all(filters={}) -> results.DeleteResult:
+        """
+        Finds and deletes all Addresss with the filters provided.
+        Returns the deleted data as a DeleteResult.
+        """
         return query.delete_all('addresses', filters)
 
     @staticmethod
     def process_raw_addr(raw: str):
+        """Converts raw address data to a JSON string."""
         processed = usaddress.tag(raw)[0]
         addr = {}
         addr["aid"] = ""  # TBD
@@ -83,6 +119,7 @@ class Address:
 
     @classmethod
     def from_json(cls, data: str):
+        """Creates an Address object from the JSON string provided."""
         obj = json_to_object(data)
         return cls(obj.aid, obj.building, obj.city, obj.state, obj.zipcode)
 
@@ -95,15 +132,24 @@ class Address:
         self.zipcode = zipcode
 
     def to_dict(self):
+        """
+        Returns the Address object as a dict.
+        Converts ObjectID to a string if found.
+        """
         # If has an ObjectId, convert to string
         if '_id' in self.__dict__:
             self.__dict__['_id'] = str(self.__dict__['_id'])
         return self.__dict__
 
     def to_json_str(self):
+        """Returns the Address object as a JSON string."""
         return object_to_json_str(self)
 
     def save(self):
+        """
+        Inserts Address object to database or updates Address if
+        same AID is found.
+        """
         # check if post already exists
         if not Address.exists({'aid': self.aid}):
             Address.insert(self.to_dict())
--- server/src/types/post.py
+++ server/src/types/post.py
@@ -3,26 +3,37 @@ from .utils import json_to_object, object_to_json_str
 from pymongo import results
 import logging
 
-"""
-Post format
-{
-    pid         : str,
-    uid         : str
-    aid         : str,
-    title       : str,
-    descr       : str
-    condition   : str,
-    list_dt     : str (%m/%d/%Y %H:%M:%S),
-    price       : str,
-    sold        : str
-}
-"""
-
 
 class Post:
+    """
+    Required fields:
+    {
+        pid         : str
+        uid         : str
+        aid         : str
+        title       : str
+        descr       : str
+        condition   : str
+        list_dt     : str (%m/%d/%Y %H:%M:%S)
+        price       : str
+        sold        : str
+    }
+    """
+
     # STATIC METHODS
     @staticmethod
     def insert(data: dict) -> None:
+        """
+        Inserts new Post to database or updates Post if
+        same PID is found.
+
+        Arguments:
+        data (dict) -- dict containing the Post information
+
+        Exceptions:
+        ValueError -- raised if data is not of type dict or if the
+                        dictionary does not contain required fields
+        """
         if type(data) != dict:
             raise TypeError(f'Cannot insert data of type{type(data)}')
 
@@ -40,41 +51,65 @@ class Post:
 
     @staticmethod
     def update(filters: dict, new_values: dict) -> None:
+        """
+        Finds a single Post with the specified filters
+        and updates them with new values.
+
+        Arguments:
+        filters    -- the Post attributes to search for
+        new_values -- the data to be added to the Post
+
+        Exceptions:
+        TypeError -- raised if filters or new_values are not of type dict
+        """
         if type(new_values) != dict:
-            raise TypeError(f'Cannot update with data of type{type(new_values)}') # noqa
+            raise TypeError(f'Cannot update with data of type{type(new_values)}')  # noqa
         if type(filters) != dict:
-            raise TypeError(f'Cannot update with filters of type{type(filters)}') # noqa
+            raise TypeError(f'Cannot update with filters of type{type(filters)}')  # noqa
 
         query.update('posts', filters, new_values)
-        logging.info(f'Updated users w/ filters {filters}')
+        logging.info(f'Updated Posts w/ filters {filters}')
 
     @staticmethod
-    def find_all(filters={}) -> list:
+    def find_all(filters={}) -> list[dict]:
+        """Returns a list of all Posts found with the filters provided ."""
         return query.find_all('posts', filters)
 
     @staticmethod
     def find_one(filters={}) -> dict:
+        """Returns a single Post dict with the filters provided."""
         return query.find_one('posts', filters)
 
     @staticmethod
     def exists(filters={}) -> bool:
+        """Returns true if a Post with the provided filters is found."""
         return query.exists('posts', filters)
 
     @staticmethod
     def count(filters={}) -> int:
+        """Counts all Posts with filters provided."""
         return query.count('posts', filters)
 
     @staticmethod
-    def delete(pid) -> results.DeleteResult:
+    def delete_one(pid) -> results.DeleteResult:
+        """
+        Finds and deletes a single Post with the filters provided.
+        Returns the deleted data as a DeleteResult.
+        """
         return query.delete_one('posts', {'pid': pid})
 
     @staticmethod
     def delete_all(filters={}) -> results.DeleteResult:
+        """
+        Finds and deletes all Posts with the filters provided.
+        Returns the deleted data as a DeleteResult.
+        """
         return query.delete_all('posts', filters)
 
     # CLASS METHODS
     @classmethod
     def from_json(cls, data: str):
+        """Creates a Post object from the JSON string provided."""
         obj = json_to_object(data)
         return cls(obj.pid, obj.uid, obj.aid, obj.title, obj.descr, obj.condition, obj.list_dt, obj.price, obj.sold)  # noqa
 
@@ -93,16 +128,23 @@ class Post:
         self.sold = sold
 
     def to_dict(self):
-        # If has an ObjectId, convert to string
+        """
+        Returns the Post object as a dict.
+        Converts ObjectID to a string if found.
+        """
         if '_id' in self.__dict__:
             self.__dict__['_id'] = str(self.__dict__['_id'])
         return self.__dict__
 
     def to_json_str(self):
+        """Returns the Post object as a JSON string."""
         return object_to_json_str(self)
 
     def save(self):
-        # check if post already exists
+        """
+        Inserts Post object to database or updates Post if
+        same PID is found.
+        """
         if not Post.exists({'pid': self.pid}):
             Post.insert(self.to_dict())
         else:
