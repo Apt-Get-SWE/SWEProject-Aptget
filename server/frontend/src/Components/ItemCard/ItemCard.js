e1af2fd8c283141bed90d829e452c5165f93549d
Alex-YuYan <alex.yan@nyu.edu>
2023-04-01 17:33:04 -0400

    Clean up some redudant code and reformed the ItemCard component

--- server/frontend/src/Components/ItemCard/ItemCard.js
+++ server/frontend/src/Components/ItemCard/ItemCard.js
@@ -1,24 +1,30 @@
-import './ItemCard.css'
+import React from 'react'
 
-const ItemCard = (props) => (
-  <div className="ItemCard">
-    <div className="Head">
-      <div className="Author">
-        <img className="Avatar" src={props.avatar} alt="avatar"/>
-        <span className="Username"> {props.username} </span>
+const ItemCard = (props) => {
+  return (
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div>
+        {
+          props.image ?
+          <img className="object-cover w-full h-56 rounded-t-lg" src={props.image} alt="item" />
+          :
+          <img className="object-cover w-full h-56 rounded-t-lg" src={`https://picsum.photos/200/300.jpg`} alt="item" />
+        }
       </div>
-    </div>
-    <img className="Thumbnail" src={props.item_photo} alt="item"/>
-    <div className="ContentBody">
-      <div className="Details">
-        <span className="BuildingName"> {props.building_name} </span>
-        <div className="TitleDisc">
-          <span className="ItemName"> {props.item_name} </span>
-          <span className="Description"> {props.description}</span>
+      <div className="px-5 pb-5 pt-2">
+        <div>
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.itemName}</h5>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="text-3xl font-bold text-gray-900 dark:text-white">{props.price}</span>
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Contact Seller</button>
         </div>
       </div>
     </div>
-  </div>
-)
+
+  )
+}
 
 export default ItemCard
+
+
--- server/frontend/src/Pages/Register/Register.js
+++ server/frontend/src/Pages/Register/Register.js
@@ -8,22 +8,22 @@ const Register = () => {
     const navigate = useNavigate();
 
     return (
-        <div class="m-10">
+        <div className="m-10">
             <Notification trigger={trigger} title="Welcome!" description="You are officially an AptGetter" actionPrompt="Go to dashboard" action={
                 () => navigate("/dashboard")
             }/>
-            <div class="lg:grid lg:grid-cols-5 lg:gap-6">
-                <div class="lg:col-span-1"></div>
-                <div class="lg:col-span-1 text-center lg:text-left">
-                    <div class="px-4 sm:px-0 pt-5">
-                        <h3 class="text-xl font-semibold leading-6 text-gray-900">Almost there...</h3>
-                        <p class="mt-1 text-lg text-gray-600">Just a few more things we need to know about you.</p>
+            <div className="lg:grid lg:grid-cols-5 lg:gap-6">
+                <div className="lg:col-span-1"></div>
+                <div className="lg:col-span-1 text-center lg:text-left">
+                    <div className="px-4 sm:px-0 pt-5">
+                        <h3 className="text-xl font-semibold leading-6 text-gray-900">Almost there...</h3>
+                        <p className="mt-1 text-lg text-gray-600">Just a few more things we need to know about you.</p>
                     </div>
                 </div>
-                <div class="mt-5 lg:col-span-2 md:mt-0">
+                <div className="mt-5 lg:col-span-2 md:mt-0">
                     <UserInfoForm setTrigger={setTrigger}/>   
                 </div>
-                <div class="lg:col-span-1"></div>
+                <div className="lg:col-span-1"></div>
             </div>
         </div>
     );
