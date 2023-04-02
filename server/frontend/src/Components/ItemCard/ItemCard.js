dbccae8b7fa21520fb52cb6c82f638e9924d744e
Oliver Li <oliverli@Olivers-MacBook-Pro.local>
2023-04-02 11:59:30 -0400

    Navbar's dashboard button now links to dashboard page

--- server/frontend/src/Components/ItemCard/ItemCard.js
+++ server/frontend/src/Components/ItemCard/ItemCard.js
@@ -26,7 +26,6 @@ const ItemCard = (props) => {
           <button onClick={toggleContactInfo} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Contact Seller</button>
         </div>
           {showContactInfo && (<div className="mt-3"> <div className="text-gray-700 dark:text-gray-300">
-          // TODO: integrate with backend to get seller info
           <span className="font-semibold">Phone:</span> {props.phone}
             </div>
             <div className="text-gray-700 dark:text-gray-300">
