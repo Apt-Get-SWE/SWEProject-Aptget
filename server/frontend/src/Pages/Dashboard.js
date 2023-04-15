4df0c40d76350956abf829d84952722b99eb16e6
TomY-Zhang <tyz212@nyu.edu>
2023-04-15 15:18:46 -0400

    removed some changes

--- server/frontend/src/Pages/Dashboard.js
+++ server/frontend/src/Pages/Dashboard.js
@@ -2,23 +2,14 @@ import React, { useState } from 'react';
 import Footer from '../Components/Footer/Footer';
 import Navbar from '../Components/NavBar/Navbar';
 import ItemInfoForm from '../Components/Forms/ItemInfoForm'; // Make sure to import ItemCard
-import axios from 'axios';
 
 const Dashboard = () => {
   const [showModal, setShowModal] = useState(false);
 
-  const userPosts = async () => {
-    // TODO get UID from user session
-    const uid = 0;
-    const res = await axios.get(`/api/posts/posts?uid=${uid}`);
-    console.log(res.data)
-    return res.data
-  };
-
   return (
     <div className="min-h-screen">
       <Navbar />
-      <div className="w-5/6 mx-auto" onMouseEnter={() => userPosts()}>
+      <div className="w-5/6 mx-auto">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
           onClick={() => setShowModal(true)}
