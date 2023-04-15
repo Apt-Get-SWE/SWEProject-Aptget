6669ffb57b245a2df1d3c6f1a1138e1747cd2044
TomY-Zhang <tyz212@nyu.edu>
2023-04-15 19:10:39 -0400

    display user's own posts on dashboard

--- server/frontend/src/Components/ItemCard/ItemCard.js
+++ server/frontend/src/Components/ItemCard/ItemCard.js
@@ -20,10 +20,10 @@ const ItemCard = (props) => {
       </div>
       <div className="px-5 pb-5 pt-2">
         <div>
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.itemName}</h5>
+          <h5 className="text-3xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.itemName}</h5>
         </div>
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">{props.price}</span>
+          <span className="text-xl font-bold text-gray-900 dark:text-white">{`$${props.price}`}</span>
           <button onClick={() => toggleContactInfo()} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{
             showContactInfo ? "Hide Info" : "Contact Seller"
 }</button>
--- server/frontend/src/Pages/Dashboard.js
+++ server/frontend/src/Pages/Dashboard.js
@@ -1,17 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../Components/Footer/Footer';
 import Navbar from '../Components/NavBar/Navbar';
 import ItemInfoForm from '../Components/Forms/ItemInfoForm'; // Make sure to import ItemCard
+import ItemCard from '../Components/ItemCard/ItemCard'
+import axios from 'axios';
 
 const Dashboard = () => {
   const [showModal, setShowModal] = useState(false);
+  const [posts, setPosts] = useState([]);
 
+  const geUserPosts = async () => {
+    const post_res = await axios.get('/api/posts/posts?user=True');
+
+    let data = [];
+    for (const [_, value] of Object.entries(post_res.data.Data)) {
+      const user_res = await axios.get(`/api/users/users?uid=${value.uid}`);
+
+      let user = user_res.data.Data[value.uid]
+      value.email = user.email
+      value.phone = user.phone
+
+      console.log(value)
+      data.push(value);
+    }
+
+    return data;
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const newPosts = await geUserPosts();
+      setPosts(newPosts);
+    };
+    fetchPosts();
+  }, []);
+  
   return (
     <div className="min-h-screen">
       <Navbar />
       <div className="w-5/6 mx-auto">
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
           onClick={() => setShowModal(true)}
         >
           Add New Item
@@ -30,6 +59,18 @@ const Dashboard = () => {
             </div>
           </>
         )}
+      </div>
+      <div className="w-5/6 mx-auto mt-7">
+        <h1 className="text-5xl font-bold mb-2">Your Posts</h1>
+        <div className="grid grid-cols-3 gap-4 w-5/6 mt-5">
+          {
+            posts.map((item, index) => (
+              <div>
+                <ItemCard key={index} email={item.email} phone={item.phone} itemName={item.title} price={item.price} />
+              </div>
+            ))
+          }
+        </div>
       </div>
       <div className="sticky top-[100vh]">
         <Footer />
