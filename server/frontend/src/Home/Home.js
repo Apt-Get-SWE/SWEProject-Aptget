fc1491006760fcd006a3ad4aba8e5680a30293af
Alex-YuYan <alex.yan@nyu.edu>
2023-04-27 10:54:09 -0400

    Implement search as login to further search

--- server/frontend/src/Home/Home.js
+++ server/frontend/src/Home/Home.js
@@ -49,8 +49,10 @@ function Home() {
                 placeholder="What's your address?"
                 className="border border-green-300 p-2 w-2/3 focus:ring-2 focus:ring-green-500 focus:outline-none z-10 rounded-full"
               />
-              <button className="bg-white hover:bg-green-200 ring-green-500 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center z-10 ml-2"> Search
-              </button>
+              <a href='/api/login/login'>
+                <button className="bg-white hover:bg-green-200 ring-green-500 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center z-10 ml-2"> Search
+                </button>
+              </a>
             </span>
           </div>
         </div>
